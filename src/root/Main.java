49683eed7587448d1d184f09f8a11e8110e08d53
janekos <janekos@users.noreply.github.com>
2018-04-03 14:42:44 +0300

    textToNgram done

--- src/root/Main.java
+++ src/root/Main.java
@@ -1,6 +1,9 @@
 package root;
 
 import root.online.Requests;
+
+import java.util.Arrays;
+
 import root.analyzer.TextToNgram;
 
 public class Main {
@@ -8,7 +11,8 @@ public class Main {
 	public static void main(String[] args) {
 		
 		//Requests.initServer(); //starts api	
-		TextToNgram.getWordTypesFromText("Tallinna Ülikooli eesti vahekeele korpus (EVKK) on eesti keele kui riigikeele (teise keele) ja võõrkeele õppijate kirjalike tekstide kogu.EVKK-s on rida alamkorpusi, kasutajaliides, mitmetasandiline annoteerimis- ja märgendussüsteem, statistikamoodul, tekstide automaatanalüüsi võimalused jm. Kombineerides erinevaid alamkorpusi, tekstilisi tunnuseid, vealiike ja metateavet õppija kohta, võimaldab korpuse kasutajaliides teostada mitmetasandilist otsingut. \r\n Korpust saab kasutada empiirilises ja rakenduslikku laadi uurimistöös; tulevaste õpetajate ja lingvistide koolitamisel; tegevõpetajate täiendõppes jm.");
+		String[] asd = TextToNgram.getWordTypesFromText("Automaatsed hindamissüsteemid aitavad suuresti vähendada hindajate ja õpetajate koormust, individualiseerivad õppimisega seotud aspekte, soodustavad iseõppimist jm, ent sellegipoolest on need süsteemid saanud ka kriitika osaliseks. Ühe miinusena on välja toodud, et need süsteemid ei ole võimelised inimlikult hindama. Teisena on märgitud, et hindamissüsteemid on liialt pinnapealsed, hinnates peamiselt esseede mõõdetavaid tegureid nagu essee pikkus ja sõnade järjestus või jättes need kõrvale ja hinnates ainult sisu. (Dikli 2006: 24-26)15 Kolmandaks probleemiks või ohuks on peetud võimalust automaatset hindamissüsteemi petta, kui selle tööpõhimõtted ja hindamise alused on teada. Hindamissüsteemide arendajad lükkavad need väited ümber või lubavad süsteeme edasi arendada, et sarnast situatsiooni vältida. Neljandaks ohuks peetakse seda, et õpilased hakkavad kirjutama „masinale” ja õpetajad õpetama „masinale” kirjutamist, sest kõik teavad, et töid hinnatakse automaatselt. Viiendaks, kui töid hinnatakse automaatselt ja on teada, millistel teguritel on essee hindamisel (järelikult ka kirjutamisel) suurem kaal, siis arvatakse, et eksamineerija võib süsteemi osaliselt petta. (Blood 2011: 55-59)16 Eespool loetletud puudused on automaatsete hindamissüsteemide arendajad kas juba ümber lükanud või lubavad süsteeme edasi arendades neid probleeme vältida. Töö autor on arvamusel, et inimfaktor ei kao esseede hindamisel kuhugi. Automaatse hindamissüsteemi ja vastavate tarkvaralahenduste eesmärk pole inimteguri kaotamine, vaid nimetatud kahe suuna sümbioos, koostoimimine, et anda keeleõppijatele ja hindajatele avaramaid võimalusi enesekontrolliks. Sellest on abi keele õppimisel, keelekorralduses, samuti keelekasutuse uurimisel.");
+		System.out.println(Arrays.toString(asd));
 		
 	}
 }
--- src/root/analyzer/TextToNgram.java
+++ src/root/analyzer/TextToNgram.java
@@ -1,9 +1,11 @@
 package root.analyzer;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.script.ScriptEngine;
@@ -14,13 +16,13 @@ public class TextToNgram {
 
 	public static String[] createNgrams(int ngramLen, String text) {
 		
-		List<String> wordTypes = getWordTypesFromText(text);
+		String[] wordTypes = getWordTypesFromText(text);
 		List<String> ngramsList = new ArrayList<String>();
 
-		for(int i = 1; i < wordTypes.size() - (ngramLen - 2); i++ ) {
+		for(int i = 1; i < wordTypes.length - (ngramLen - 2); i++ ) {
 			String ngram = "";
 
-			for(int j = -1; j < ngramLen-2; j++) { ngram += wordTypes.get(i+j); }
+			for(int j = -1; j < ngramLen-2; j++) { ngram += wordTypes[i+j]; }
 
 			ngramsList.add(ngram);
 		}
@@ -30,27 +32,38 @@ public class TextToNgram {
 		return ngramsArray;
 	}
 
-	public static List<String> getWordTypesFromText(String text){
+	public static String[] getWordTypesFromText(String text){
 
-		List<String> wordTypes = new ArrayList<String>();
+		String[] wordTypes = null;
 		String s = null;
-		System.out.println("siin1");
+		
 		//https://alvinalexander.com/java/edu/pj/pj010016
 		try {
-			//Process p = new ProcessBuilder().command(new String[] {"cmd", "python.exe text_to_postags.py"}).start();
-			String[] command = new String[] {"python", "text_to_postags.py", "\""+text+"\""};
-			Process p = new ProcessBuilder(command).start();
+			Process p = null;
+			ProcessBuilder pb = new ProcessBuilder("python","text_to_postag.py","\""+text+"\"");
+			pb.directory(new File(System.getProperty("user.dir")));
+			p = pb.start();
+			
 			BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			System.out.println("siin2");
-			System.out.println(stdInput.readLine());
-			while ((s = stdInput.readLine()) != null) {
+			
+			s = stdInput.readLine();
+			if(!s.equals("ErrNo1")) {
+				s = s.replace("[", "").replace("]", "").replace(" ", "");
+				wordTypes = s.split(",");
+			}
+			/*while ((s = stdInput.readLine()) != null) {
 				//implement writing into wordTypes list
 				System.out.println(s);
-			}
+			}*/
 			
-			System.out.println("siin3");
-		} catch (IOException e) { System.out.println(e); System.out.println("nahka");}
-		System.out.println("siin4");
+			/*BufferedReader stdError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
+			
+			while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }*/
+			
+		} catch (IOException e) { System.out.println(e); }
+
 		return wordTypes; //returns list of each words postag
 
 	}	
